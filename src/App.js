bb20d86faddddacc114223124d5bac6a63ca497e
Ian McFarlane <imackintouch@gmail.com>
2022-05-30 18:15:46 -0400

    May 2021 code last working version pre assignment

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expenseData) => {
+    console.log("In App.js")
+    console.log(expenseData);
+  }
+
   // return React.createElement(
   //   "div",
   //   {},
@@ -34,7 +39,7 @@ const App = () => {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   // document.getElementById('').addEventListener('click', (event) => {});
 
   const [enteredTitle, setEnteredTitle] = useState("");
@@ -37,7 +37,9 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(ExpenseData);
+    
+    //console.log(ExpenseData);
+    props.onSaveExpenseData(ExpenseData)
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
